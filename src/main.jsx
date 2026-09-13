ac1c1755f75469c9d7c15d85abc8b62132343d1d
raismrakotoniaina-hash <raismrakotoniaina@gmail.com>
2026-09-13 14:28:29 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
-function App() {
-  return (
-    <div>
-      <h1>N-AI Chat V2</h1>
-      <p>Bienvenue sur N-AI Chat V2</p>
-    </div>
-  );
-}
+import App from "./App";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-);
+); 
