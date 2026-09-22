1774f986d5cc15f48c5807af8329aa17b03c0b3b
raismrakotoniaina-hash <raismrakotoniaina@gmail.com>
2026-09-22 14:48:38 +0300

    Connect topbar profile navigation

--- src/App.jsx
+++ src/App.jsx
@@ -501,6 +501,9 @@ function App() {
           onMenuOpen={() =>
             setSidebarOpen(true)
           }
+          onProfileClick={() =>
+            handleNavigate("profile")
+          }
         />
 
         {renderPage()}
